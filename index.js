5c7ac3094dce698f0a7238f7dee7148a346f0fbe
Jacob Adams <jacob.adams121202@gmail.com>
2025-05-09 23:10:49 -0700

    Challenge 5 Complete

--- index.js
+++ index.js
@@ -98,8 +98,11 @@ function filterUnfundedOnly() {
   deleteChildElements(gamesContainer);
 
   // use filter() to get a list of games that have not yet met their goal
-
+  let filteredGames = GAMES_JSON.filter((game) => {
+    return game.pledged < game.goal;
+  });
   // use the function we previously created to add the unfunded games to the DOM
+  addGamesToPage(filteredGames);
 }
 
 // show only games that are fully funded
@@ -107,8 +110,11 @@ function filterFundedOnly() {
   deleteChildElements(gamesContainer);
 
   // use filter() to get a list of games that have met or exceeded their goal
-
+  let filteredGames = GAMES_JSON.filter((game) => {
+    return game.pledged >= game.goal;
+  });
   // use the function we previously created to add unfunded games to the DOM
+  addGamesToPage(filteredGames);
 }
 
 // show all games
@@ -116,12 +122,16 @@ function showAllGames() {
   deleteChildElements(gamesContainer);
 
   // add all games from the JSON data to the DOM
+  addGamesToPage(GAMES_JSON);
 }
 
 // select each button in the "Our Games" section
 const unfundedBtn = document.getElementById("unfunded-btn");
+unfundedBtn.addEventListener("click", filterUnfundedOnly);
 const fundedBtn = document.getElementById("funded-btn");
+fundedBtn.addEventListener("click", filterFundedOnly);
 const allBtn = document.getElementById("all-btn");
+allBtn.addEventListener("click", showAllGames);
 
 // add event listeners with the correct functions to each button
 
